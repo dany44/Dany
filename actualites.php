9059986a58da9ab95c8c4a50238ba93058c87487
dany44 <danykhadhar@hotmail.fr>
2015-02-06 11:47:52 +0100

    ajout de la date

--- actualites.php
+++ actualites.php
@@ -5,5 +5,7 @@
 <p>Formation des barbares et des dragons 5 fois plus rapide! (Duree limitee)
 <p>Edit: Souvent Clash of Clans fait un evenement special concernant une troupe (ou plusieurs) avant d annoncer une mise a jour!
 <a href="https://www.facebook.com/frclashofclans">citation</a>
+<div class="date">29/1/2015<br>
+</div>
 
 <?php include( 'footer.php' );?>
